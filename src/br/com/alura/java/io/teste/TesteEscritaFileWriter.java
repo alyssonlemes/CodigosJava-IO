900cc09f1166a34bdb7f4a395653515478c3ef57
Alysson Lemes <65565603+alyssonlemes@users.noreply.github.com>
2022-12-26 16:54:01 -0300

    Update TesteEscritaFileWriter.java

--- src/br/com/alura/java/io/teste/TesteEscritaFileWriter.java
+++ src/br/com/alura/java/io/teste/TesteEscritaFileWriter.java
@@ -1,42 +1,25 @@
 package br.com.alura.java.io.teste;
+
 import java.io.BufferedWriter;
-import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.PrintStream;
-import java.io.PrintWriter;
-import java.io.Writer;
 
 public class TesteEscritaFileWriter {
 
 	public static void main(String[] args) throws IOException {
-
-		//Fluxo de entrada com um arquivo
 		
+		//Fluxo de Entrada com Arquivo
 //		OutputStream fos = new FileOutputStream("lorem2.txt");
 //		Writer osw = new OutputStreamWriter(fos);
 //		BufferedWriter bw = new BufferedWriter(osw);
 		
-//		BufferedWriter bw = new BufferedWriter(new FileWriter("lorem2.txt"));
-		
-//		PrintStream ps = new PrintStream(new File("lorem2.txt")); // para imprimir caracteres no arquivo
-		
-		PrintWriter ps = new PrintWriter("lorem2.txt");
-		
-		ps.println("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod");
-		ps.println();
-		ps.println();
-		ps.println();
-		ps.println();
-		ps.println("asdfadsfdafd");
-		
-		
-		ps.close();
-		
-		
+		BufferedWriter bw = new BufferedWriter(new FileWriter("lorem2.txt"));
+		bw.write("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod");
+		bw.newLine();
+		bw.newLine();
+		bw.write("asfasdfsafdas dfs sdf asf assdß");
+				
+		bw.close();
 		
 	}
-
 }
