102bc4a562a1b11372fff43926cf97089afb4feb
Alysson Lemes <65565603+alyssonlemes@users.noreply.github.com>
2023-01-03 22:09:01 -0300

    Update TesteUnicodeEEncoding.java

--- src/br/com/alura/java/io/teste/TesteUnicodeEEncoding.java
+++ src/br/com/alura/java/io/teste/TesteUnicodeEEncoding.java
@@ -7,27 +7,58 @@ import java.nio.charset.StandardCharsets;
 public class TesteUnicodeEEncoding {
 
 	public static void main(String[] args) throws UnsupportedEncodingException {
-		
-		String s = "ç";
-		System.out.println(s.codePointAt(0));//unicode
+
+		String s = "C";
+		System.out.println(s.codePointAt(0));
 		
 		Charset charset = Charset.defaultCharset();
 		System.out.println(charset.displayName());
 		
 		byte[] bytes = s.getBytes("windows-1252");
-		System.out.print(bytes.length + ", windows-1252, ");
-		String sNovo = new String(bytes, "windows-1252");
-		System.out.println(sNovo);
+		System.out.println(bytes.length + " , windows-1252");
+		String snovo = new String(bytes, "windows-1252");
+		System.out.println(snovo);
 		
 		bytes = s.getBytes("UTF-16");
-		System.out.print(bytes.length + ", UTF-16, ");
-		sNovo = new String(bytes, "UTF-16");
-		System.out.println(sNovo);
+		System.out.print(bytes.length + " , UTF-16,   ");
+		snovo = new String(bytes, "UTF-16");
+		System.out.println(snovo);
 		
 		bytes = s.getBytes(StandardCharsets.US_ASCII);
-		System.out.print(bytes.length + ", US-ASCII, ");
-		sNovo = new String(bytes, "US-ASCII");
-		System.out.println(sNovo);
+		System.out.print(bytes.length + " , US_ASCII,    ");
+		snovo = new String(bytes, "US_ACSII");
+		System.out.println(snovo);
+		
+	
+		//TESTANDO
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	}
 
 }
